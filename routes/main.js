5e22555be9f3ebf23a3872bdf5c356141b5c5960
ChoJaeHee <cbw1030@naver.com>
2020-02-14 17:14:11 +0900

    로그인화면 구성 및 라우팅 처리

--- routes/main.js
+++ routes/main.js
@@ -5,7 +5,17 @@ const  router   = express.Router();
 
 // 메인화면을 출력합니다.
 const  GetMainUI = (req, res) => {   
-   
+    let htmlStream = ''; 
+
+    htmlStream = htmlStream + fs.readFileSync(__dirname + '/../views/header.ejs','utf8');  // 초기설정(부트스트랩/제이쿼리 등)
+    
+    htmlStream = htmlStream + fs.readFileSync(__dirname + '/../views/main.ejs','utf8'); // Content
+    htmlStream = htmlStream + fs.readFileSync(__dirname + '/../views/footer.ejs','utf8');  // Footer
+
+    res.writeHead(200, {'Content-Type':'text/html; charset=utf8'}); // 200은 성공
+    res.end(ejs.render(htmlStream, {
+                                        'title' : '업무관리 프로그램',
+                                        'url' : '../' })); 
 };
 
 router.get('/', GetMainUI);
--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,16 @@ const  router   = express.Router();
 
 // 회원 로그인 화면을 출력합니다.
 const GetLoginPage = (req, res) => {
+    let htmlStream = ''; 
 
+    htmlStream = htmlStream + fs.readFileSync(__dirname + '/../views/header.ejs','utf8');  // 초기설정(부트스트랩/제이쿼리 등)
+    htmlStream = htmlStream + fs.readFileSync(__dirname + '/../views/login.ejs','utf8'); // Content
+    htmlStream = htmlStream + fs.readFileSync(__dirname + '/../views/footer.ejs','utf8');  // Footer
+
+    res.writeHead(200, {'Content-Type':'text/html; charset=utf8'}); // 200은 성공
+    res.end(ejs.render(htmlStream, {
+                                        'title' : '업무관리 프로그램',
+                                        'url' : '../' })); 
 };
 // 로그인을 처리합니다.
 const HandleLogin = (req, res) => {
